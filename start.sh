847285f733f13186d95daba8db10ffae4e40e494
jiju <juanrsanchislopez@gmail.com>
2025-02-17 20:25:06 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # Script de arranque para WiFiAutoPwn
 
-INTERFACE="wlan0"
+echo "[*] Directorio actual: $(pwd)"
 
-if [[ $1 ]]; then
+# Verifica que el archivo wifi_auto_pwn.py exista en el directorio actual
+if [ ! -f \"wifi_auto_pwn.py\" ]; then
+    echo \"[!] Error: No se encontró 'wifi_auto_pwn.py' en el directorio actual.\"
+    exit 1
+fi
+
+# Define la interfaz por defecto, pero permite pasarla como argumento
+INTERFACE=\"wlan0\"
+if [[ $# -ge 1 ]]; then
     INTERFACE=$1
 fi
 
-echo "[*] Iniciando WiFiAutoPwn en la interfaz $INTERFACE..."
-python3 wifi_auto_pwn.py $INTERFACE
+echo \"[*] Iniciando WiFiAutoPwn en la interfaz $INTERFACE...\"
+python3 wifi_auto_pwn.py \"$INTERFACE\"
